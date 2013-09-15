a3834ea9245907b63d5b755fc4993637bb0c3e1a
Valtis <erkka_kaaria@outlook.com>
2013-09-15 14:03:59 +0300

    Testing spaceship-like controls. Adding data files etc. to source control.

--- Moottori/src/Component/LocationComponent.cpp
+++ Moottori/src/Component/LocationComponent.cpp
@@ -1,7 +1,7 @@
 #include "Component/LocationComponent.h"
 #include "Event/ChangeLocationEvent.h"
 #include <SDL_assert.h>
-
+#include <cmath>
 void LocationComponent::OnEventHandlerRegistration()
 {
 	GetEventHandler().RegisterCallback(EventType::ChangeLocation, [&](Event *event) { this->HandleLocationChangeEvents(event); } );
@@ -15,4 +15,11 @@ void LocationComponent::HandleLocationChangeEvents(Event *event)
 	mX += locationEvent->GetXChange();
 	mY += locationEvent->GetYChange();
 	mRotation += locationEvent->GetRotationChange();
+
+	// experimenting here
+	/*int velocity = locationEvent->GetYChange();
+	double angle = mRotation*3.1415926535/180.0;
+	mX += sin(angle)*velocity;
+	mY += -cos(angle)*velocity;*/
+
 }
