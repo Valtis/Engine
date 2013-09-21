efc474ffcd8d940c469a283da623638339797b95
Valtis <erkka_kaaria@outlook.com>
2013-09-21 07:26:19 +0300

    One day, I'll remember to commit script files as well (and figure out where the amend commit button is). Today is not that day

--- Moottori/data/scripts/ship_velocity.lua
+++ Moottori/data/scripts/ship_velocity.lua
@@ -1,18 +1,25 @@
-
+function SigNum(value)
+	if value < 0 then
+		return -1
+	elseif value > 0 then
+		return 1
+	else
+		return 0
+	end
+	
+end
 
 function DecaySpeed()
 	
 	velocity_loss_per_tick = 0.3
 	rotation_velocity_loss_per_tick = 1
-
+	
 	x_velocity = velocity_component.x_velocity
 	y_velocity = velocity_component.y_velocity
-	rotation_velocity =  velocity_component.rotation_velocit
+	rotation_velocity =  velocity_component.rotation_velocity
 
 	angle = math.atan2(y_velocity, x_velocity)
-
-	
-	totalVelocity = math.sqrt(velocity_component.x_velocity*velocity_component.x_velocity + velocity_component.y_velocity*velocity_component.y_velocity) - velocity_loss_per_tick
+	totalVelocity = math.sqrt(x_velocity*x_velocity + y_velocity*y_velocity) - velocity_loss_per_tick
 	
 	if totalVelocity < 0 then
 		totalVelocity = 0
@@ -21,22 +28,16 @@ function DecaySpeed()
 	velocity_component.x_velocity = math.abs(totalVelocity)*math.cos(angle)
 	velocity_component.y_velocity = math.abs(totalVelocity)*math.sin(angle)
 
-	rotSign = 0
-	if velocity_component.rotation_velocity < 0 then
-		rotSign = -1
-	else
-		rotSign = 1
-	end
+	rotSign = SigNum(rotation_velocity)
 	
-	velocity_component.rotation_velocity = math.abs(velocity_component.rotation_velocity) - rotation_velocity_loss_per_tick
-	if velocity_component.rotation_velocity < 0 then
-		velocity_component.rotation_velocity = 0
+	rotation_velocity = math.abs(rotation_velocity) - rotation_velocity_loss_per_tick
+	if rotation_velocity < 0 then
+		rotation_velocity = 0
 	end
 
-	velocity_component.rotation_velocity = rotSign*velocity_component.rotation_velocity
+	velocity_component.rotation_velocity = rotSign*rotation_velocity
 	
 end
-
 
 function OnUpdate(ticks_passed)
 	
@@ -49,3 +50,29 @@ function OnUpdate(ticks_passed)
 	velocity_component:SendAnimationStateEvent(animation_state)
 end
 
+function OnVelocityChangeEvent(x_velocity_change, y_velocity_change, rotation_velocity_change)
+	
+	max_velocity = 8
+	max_rotation_speed = 8
+	
+	velocity_component.x_velocity = velocity_component.x_velocity + x_velocity_change
+	velocity_component.y_velocity = velocity_component.y_velocity + y_velocity_change
+	velocity_component.rotation_velocity = velocity_component.rotation_velocity + rotation_velocity_change
+
+	x_sign = SigNum(velocity_component.x_velocity)
+	y_sign = SigNum(velocity_component.y_velocity)
+	rotate_sign = SigNum(velocity_component.rotation_velocity)
+
+	if math.abs(velocity_component.x_velocity) > max_velocity then
+		velocity_component.x_velocity = max_velocity*x_sign
+	end
+
+	if math.abs(velocity_component.y_velocity) > max_velocity then
+		velocity_component.y_velocity = max_velocity*y_sign
+	end
+
+	if math.abs(velocity_component.rotation_velocity) > max_rotation_speed then
+		velocity_component.rotation_velocity = max_rotation_speed*rotate_sign
+	end
+
+end
